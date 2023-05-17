0a9a6afbe1e2c9c29723df3a69d58c92eabd68ee
longlivedrgn <kimye9808@naver.com>
2023-05-17 19:03:13 +0900

    feat: gitignore 파일 생성

---
+++ MiroCinema/MiroCinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiroCinema
+//
+//  Created by 김용재 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
