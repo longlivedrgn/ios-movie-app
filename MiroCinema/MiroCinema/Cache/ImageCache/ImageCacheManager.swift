2e0c8e9576fa1b2a3536fb4c99b444048857eebe
longlivedrgn <kimye9808@naver.com>
2023-06-08 16:40:54 +0900

    feat: ImageCacheManager 타입 정의

--- MiroCinema/MiroCinema/Cache/ImageCache/ImageCacheManager.swift
+++ MiroCinema/MiroCinema/Cache/ImageCache/ImageCacheManager.swift
@@ -7,9 +7,47 @@
 
 import UIKit
 
-final class ImageCacheManager {
+final class ImageCacheManager  {
 
-    static let shared = NSCache<NSString, UIImage>()
-    private init() {}
+    static let shared = ImageCacheManager()
+
+    private let diskStorage = DiskStorage()
+    private let memoryStorage = MemoryStorage()
+
+    enum Storage {
+        case disk
+        case memory
+    }
+
+    func isCached(resourceKey key: String) -> Bool {
+        let cacheKey = cacheKey(of: key)
+        guard memoryStorage.isCached(forKey: cacheKey) || diskStorage.isCached(forkey: cacheKey) else {
+            return false
+        }
+        return true
+    }
+
+    private func cacheKey(of resourceKey: String) -> String {
+        return resourceKey.components(separatedBy: ["/"]).last!
+    }
+
+    func store(image: UIImage, forResourceKey key: String, in storage: Storage) {
+        let cacheKey = cacheKey(of: key)
+        switch storage {
+        case .disk :
+            diskStorage.store(image: image, forKey: cacheKey)
+        case .memory:
+            memoryStorage.store(image: image, forKey: cacheKey)
+        }
+    }
+
+    func value(forResoureceKey key: String) -> UIImage {
+        let cacheKey = cacheKey(of: key)
+        if memoryStorage.isCached(forKey: cacheKey) {
+            return memoryStorage.value(forKey: cacheKey) ?? UIImage()
+        } else {
+            return diskStorage.value(forKey: cacheKey)
+        }
+    }
 
 }
