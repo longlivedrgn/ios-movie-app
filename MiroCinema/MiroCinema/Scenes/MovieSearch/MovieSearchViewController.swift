dd4c50d430ed0754c91791f61267c307f76704ba
longlivedrgn <kimye9808@naver.com>
2023-06-09 16:51:44 +0900

    feat: MovieSearchViewController - collection view 기본 구현

--- MiroCinema/MiroCinema/Scenes/MovieSearch/MovieSearchViewController.swift
+++ MiroCinema/MiroCinema/Scenes/MovieSearch/MovieSearchViewController.swift
@@ -9,17 +9,43 @@ import UIKit
 
 class MovieSearchViewController: UIViewController {
 
+    private enum Section {
+        case main
+    }
+
     private let movieNetworkAPIManager = NetworkAPIManager()
     private let movieNetworkDispatcher = NetworkDispatcher()
 
+    private lazy var searchCollectionView: UICollectionView = {
+        let collectionview = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionview.backgroundColor = .black
+        collectionview.delegate = self
+        collectionview.register(cell: MovieGenreListCell.self)
+
+        return collectionview
+    }()
+
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Movie>
+    private typealias SnapShot = NSDiffableDataSourceSnapshot<Section, Movie>
+
+    private var datasource: DataSource?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureView()
+        configureViews()
         configureSearchBar()
     }
 
-    private func configureView() {
+    private func configureViews() {
         view.backgroundColor = .green
+        configureCollectionView()
+    }
+
+    private func configureCollectionView() {
+        view.addSubview(searchCollectionView)
+        searchCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     private func configureSearchBar() {
@@ -28,6 +54,52 @@ class MovieSearchViewController: UIViewController {
         searchBar.delegate = self
         navigationItem.titleView = searchBar
     }
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { section, layoutEnvironment in
+
+            let movieItemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalWidth(0.35)
+            )
+
+            let movieItem = NSCollectionLayoutItem(layoutSize: movieItemSize)
+
+            movieItem.contentInsets = NSDirectionalEdgeInsets(
+                top: 10,
+                leading: 10,
+                bottom: 10,
+                trailing: 10
+            )
+
+            let movieGroupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalWidth(1/3)
+            )
+
+            let movieGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: movieGroupSize,
+                subitems: [movieItem, movieItem, movieItem]
+            )
+
+            movieGroup.contentInsets = NSDirectionalEdgeInsets(
+                top: 0,
+                leading: 0,
+                bottom: 0,
+                trailing: 20
+            )
+
+            let movieSection = NSCollectionLayoutSection(group: movieGroup)
+
+            return movieSection
+        }
+
+        return layout
+    }
+
+}
+
+extension MovieSearchViewController: UICollectionViewDelegate {
 
 }
 
